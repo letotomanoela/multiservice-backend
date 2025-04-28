41cacc03e1ce65cff819e35c110eff003de56141
letotomanoela <letotomanoela@gmail.com>
2025-04-28 21:40:37 +0300

    feat: enrich appealRequest API with employee and HR advisor data retrieval

--- appealRequest/src/index.ts
+++ appealRequest/src/index.ts
@@ -1,10 +1,137 @@
 import { Hono } from "hono";
 import db from "./db";
+import { appealRequest } from "./db/schema";
+import { eq } from "drizzle-orm";
 
 const app = new Hono();
 
 app.get("/api/appealRequest", async (c) => {
   const data = await db.query.appealRequest.findMany();
+  const fullData: any[] = [];
+  try {
+    const getEmployes = await fetch("http://localhost:4000/api/employes");
+    const getHrAdvisor = await fetch("http://localhost:4000/api/hrAdvisor");
+    const employes = await getEmployes.json();
+    const hrAdvisor = await getHrAdvisor.json();
+
+    data.map((item) => {
+      const employesData = employes.find(
+        (employes: any) => employes.employesId === item.employesId
+      );
+      const hrAdvisorData = hrAdvisor.find(
+        (hrAdvisor: any) => hrAdvisor.hrAdvisorId === item.hrAdvisorId
+      );
+      fullData.push({
+        ...item,
+        employes: employesData,
+        hrAdvisor: hrAdvisorData,
+      });
+    });
+  } catch (error) {
+    data.map((item) => {
+      fullData.push({
+        ...item,
+        employes: "Could not fetch data",
+        hrAdvisor: "Could not fetch data",
+      });
+    });
+  }
+
+  return c.json(fullData);
+});
+
+app.get("/api/appealRequest/:id", async (c) => {
+  const id = c.req.param("id");
+  const data = await db.query.appealRequest.findFirst({
+    where: (appealRequest, { eq }) => eq(appealRequest.requestId, id),
+  });
+
+  let fullData = {};
+
+  try {
+    const getEmployes = await fetch(
+      `http://localhost:4000/api/employes/${data?.employesId}`
+    );
+    const getHrAdvisor = await fetch(
+      `http://localhost:4000/api/hrAdvisor/${data?.hrAdvisorId}`
+    );
+    const employes = await getEmployes.json();
+    const hrAdvisor = await getHrAdvisor.json();
+    fullData = {
+      ...data,
+      employes,
+      hrAdvisor,
+    };
+  } catch (error) {
+    fullData = {
+      ...data,
+      employes: "Could not fetch data",
+      hrAdvisor: "Could not fetch data",
+    };
+  }
+
+  return c.json(fullData);
+});
+
+app.post("/api/appealRequest", async (c) => {
+  const data = await c.req.json();
+  const newData = await db.insert(appealRequest).values(data);
+  // Verify employesId and hrAdvisorId
+  const getEmployes = await fetch(
+    `http://localhost:4000/api/employes/${data.employesId}`
+  );
+
+  const employes = await getEmployes.json();
+  if (!employes) {
+    return c.json({ error: "Employes not found" }, 404);
+  }
+
+  return c.json(newData);
+});
+
+app.put("/api/appealReaquest/approve/:id", async (c) => {
+  const id = c.req.param("id");
+  const body = await c.req.json();
+
+  // Verify hrAdvisorId
+  const getHrAdvisor = await fetch(
+    `http://localhost:4000/api/hrAdvisor/${body.hrAdvisorId}`
+  );
+  const hrAdvisor = await getHrAdvisor.json();
+  if (!hrAdvisor) {
+    return c.json({ error: "HrAdvisor not found" }, 404);
+  }
+
+  const data = await db
+    .update(appealRequest)
+    .set({
+      requestStatus: "approved",
+      hrAdvisorId: body.hrAdvisorId,
+    })
+    .where(eq(appealRequest.requestId, id));
+  return c.json(data);
+});
+
+app.put("/api/appealReaquest/reject/:id", async (c) => {
+  const id = c.req.param("id");
+  const body = await c.req.json();
+  // Verify hrAdvisorId
+  const getHrAdvisor = await fetch(
+    `http://localhost:4000/api/hrAdvisor/${body.hrAdvisorId}`
+  );
+  const hrAdvisor = await getHrAdvisor.json();
+  if (!hrAdvisor) {
+    return c.json({ error: "HrAdvisor not found" }, 404);
+  }
+
+  const data = await db
+    .update(appealRequest)
+    .set({
+      requestStatus: "rejected",
+      hrAdvisorId: body.hrAdvisorId,
+    })
+    .where(eq(appealRequest.requestId, id));
+
   return c.json(data);
 });
 
