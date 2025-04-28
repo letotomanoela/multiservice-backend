0bdc0ee6a0f9d335c3406f7cf30b1253b3a9541a
letotomanoela <letotomanoela@gmail.com>
2025-04-28 21:52:00 +0300

    refactor: remove redundant express.json() middleware call

--- users/src/app.ts
+++ users/src/app.ts
@@ -6,14 +6,11 @@ import employeRoute from "./routes/employe.route";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-console.log("DB_URL", process.env.DATABASE_URL);
+app.use(express.json());
 
 app.use("/api/beneficiary", beneficiaryRoute);
 app.use("/api/hrAdvisor", hrAdvisorRoute);
 app.use("/api/employe", employeRoute);
-
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log("USERS SERVICES RUNNING ON PORT " + PORT);
