bb937e5f46d0ead6b01f3b4527683d449bd273c3
letotomanoela <letotomanoela@gmail.com>
2025-04-28 21:46:12 +0300

    feat: enrich changeRequest API with HR advisor data retrieval

--- changeRequest/src/index.ts
+++ changeRequest/src/index.ts
@@ -9,7 +9,64 @@ app.get("/api/changeRequest", async (c) => {
       informations: true,
     },
   });
-  return c.json(data);
+
+  const fullData: any[] = [];
+
+  try {
+    // get the hrAdvisors
+    const getHrAdvisor = await fetch("http://localhost:4000/api/hrAdvisor");
+    const hrAdvisor = await getHrAdvisor.json();
+
+    data.map((item) => {
+      const hrAdvisorData = hrAdvisor.find(
+        (hrAdvisor: any) => hrAdvisor.hrAdvisorId === item.hrAdvisorId
+      );
+      fullData.push({
+        ...item,
+        hrAdvisor: hrAdvisorData,
+      });
+    });
+  } catch (error) {
+    data.map((item) => {
+      fullData.push({
+        ...item,
+        hrAdvisor: "Could not fetch data",
+      });
+    });
+  }
+
+  return c.json(fullData);
+});
+
+app.get("/api/changeRequest/:id", async (c) => {
+  const id = c.req.param("id");
+  const data = await db.query.changeRequest.findFirst({
+    where: (changeRequest, { eq }) => eq(changeRequest.changeRequestId, id),
+    with: {
+      informations: true,
+    },
+  });
+
+  let fullData = {};
+
+  try {
+    // get the hrAdvisors
+    const getHrAdvisor = await fetch(
+      `http://localhost:4000/api/hrAdvisor/${data?.hrAdvisorId}`
+    );
+    const hrAdvisor = await getHrAdvisor.json();
+    fullData = {
+      ...data,
+      hrAdvisor,
+    };
+  } catch (error) {
+    fullData = {
+      ...data,
+      hrAdvisor: "Could not fetch data",
+    };
+  }
+
+  return c.json(fullData);
 });
 
 export default app;
